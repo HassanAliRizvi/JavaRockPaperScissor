cb1e41ef7de66b5964d54f51e214015ad92d6571
HassanAliRizvi <hasanijthedi2002@gmail.com>
2022-02-25 14:38:25 -0500

    new updates. Need to figure out bug

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -17,46 +17,61 @@ function computerPlay(){
     return compSelection
 }
 
-function playRound() {
-    let playerSelection = this.id;
-    console.log(this.id)
+
+function playRound(playerSelection) {
     let computerSelection = computerPlay()
-    if(computerSelection == playerSelection) {
-        console.log("That's a draw!")
+    let computerPoints = 0;
+    let playerPoints = 0;
+
+    if(playerSelection == "Rock" && computerSelection=="Scissors" ||
+        playerSelection == "Paper" && computerSelection=="Rock" ||
+        playerSelection == "Scissors" && computerSelection=="Paper") {
+
+            playerPoints = playerPoints + 1; //PLAYER POINTS INCREMENT
+
+            results.innerHTML = playerSelection + " beats " + computerSelection + "<br>Player Score: " + playerPoints +"</br>"+ "<br>Computer  Score: " + computerPoints;
+            if(playerPoints == 5){
+                results.textContent = "You Win!"
+            }
+        }
+    
+    else if (computerSelection == playerSelection){
+        results.innerHTML = "That's a draw!" + "<br>Player Score: " + playerPoints +"</br>"+ "<br>Computer  Score: " + computerPoints;
     }
     
-    if(computerSelection == "Rock") {
-        if(playerSelection == "Scissors")
-            console.log("You lose! Rock beats Scissor")
-        if(playerSelection == "Paper")
-            console.log("You win! Rock beats Paper")
+    else{
+
+        computerPoints = computerPoints + 1;//COMPUTER POINTS INCREMENT
+
+        results.innerHTML = computerSelection + " beats " + playerSelection + 
+        "<br>Player Score: " + playerPoints +"</br>"+ "<br>Computer  Score: " + computerPoints;
+        if(computerPoints == 5)
+            results.textContent = "You Lose! However, Computer Wins!"
+
     }
 
-    else if(computerSelection == "Paper") {
-        if(playerSelection == "Scissors")
-            console.log("You Win! Scissor beats Paper")
-        if(playerSelection == "Rock") 
-            console.log("You Lose! Paper beats Rock")
-    }
-
-    else if(computerSelection == "Scissors") {
-        if(playerSelection == "Paper")
-            console.log("You Lose! Scissor beast Paper")
-        if(playerSelection == "Rock") 
-            console.log("You Win! Paper beats Rock")
-  }
-
-    else {
-        console.log("Invalid Response! You lost a try!")
-    }
 
 }
+
 
 
 
 // Create three buttons, one for each selection. Add an event listener to
 // the buttons that call your playRound function with the correct
 // playerSelection every time a button is clicked. (you can keep the console.logs for this step)
-document.getElementById('Rock').onclick = playRound;
-document.getElementById('Paper').onclick = playRound;
-document.getElementById('Scissors').onclick = playRound;
+const buttons = document.querySelectorAll('button');
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+      playRound(button.id);
+    });
+  });
+
+
+
+//Add a div for displaying results and change all of your console.logs into DOM methods.
+//Display the running score, and announce a winner of the game once one player reaches 5 points.
+const results = document.querySelector('.results');
+
